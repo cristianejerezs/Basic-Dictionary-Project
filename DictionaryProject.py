b29bb89220bd08d6182ae1494870d8f95d082d00
Cristian <cristianejerezs.0@gmail.com>
2020-06-25 12:59:46 -0400

    Added SpellCheck

--- DictionaryProject.py
+++ DictionaryProject.py
@@ -6,6 +6,8 @@ data = json.load(open("076 data.json"))
 
 word = str(input("Please input a word or vague description:").casefold())
 
+word = Speller.autocorrect_word(Speller(lang="en"),word)
+
 def LookUp(word):
     error = "Sorry, i cant do this"
     print(data.get(word, error))
