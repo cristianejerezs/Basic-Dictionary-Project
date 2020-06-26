585fcae8946bff6241fa1ce28eb0e8188a83c7a2
Cristian <cristianejerezs.0@gmail.com>
2020-06-26 13:05:00 -0400

    Creating a Spellcheck w/ SequenceMatcher

--- DictionaryProject.py
+++ DictionaryProject.py
@@ -1,15 +1,15 @@
 import json
-from autocorrect import Speller
 from difflib import SequenceMatcher
 
-data = json.load(open("076 data.json"))
+data = json.load(open("DicData.json"))
 
 word = str(input("Please input a word or vague description:").casefold())
 
-word = Speller.autocorrect_word(Speller(lang="en"),word)
-
 def LookUp(word):
+
     error = "Sorry, i cant do this :c "
     print(data.get(word, error))
 
 LookUp(word)
+
+SequenceMatcher(None,"rainn",data.keys())
