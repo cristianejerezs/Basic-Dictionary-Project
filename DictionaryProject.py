b2bf7c7a91bf47c076bd19490a5833853ad94514
Cristian <cristianejerezs.0@gmail.com>
2020-06-26 15:50:32 -0400

    Super dirty and messy aplication of SeqMatch w/ bugs

--- DictionaryProject.py
+++ DictionaryProject.py
@@ -1,15 +1,23 @@
 import json
 from difflib import SequenceMatcher
-
+from difflib import get_close_matches
 data = json.load(open("DicData.json"))
 
 word = str(input("Please input a word or vague description:").casefold())
 
-def LookUp(word):
+def LookUp2(word):
+    if data.__contains__(word):
+        return print(str(data[word]).strip("[]").replace("',","\n"))
+    elif not data.__contains__(word):
+        count=0
+        print("Did you mean?:")
+        for i in get_close_matches(word,data.keys()):
+            count+=1
+            print(str(count )+i)
+        correction = input("1,2,3: " )
+        if data.__contains__(get_close_matches(word,data.keys())[int(correction)-1]):
+            print(str(data[get_close_matches(word,data.keys())[int(correction)-1]]))
+        elif not data.__contains__(get_close_matches(word,data.keys())[int(correction)-1]):
+            print("Sorry, i can't do this :C")
 
-    error = "Sorry, i cant do this :c "
-    print(data.get(word, error))
-
-LookUp(word)
-
-SequenceMatcher(None,"rainn",data.keys())
+LookUp2(word)
