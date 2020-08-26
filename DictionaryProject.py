a0ffd35d9f1a0ae724523646d86cd06106644cd6
Cristian <cristianejerezs.0@gmail.com>
2020-08-25 20:46:09 -0400

    If word doesnt exists, prompts user to select one from 3 possibilities

--- DictionaryProject.py
+++ DictionaryProject.py
@@ -10,7 +10,10 @@ def LookUp(word):
     if word in data:
         return data[word]
     elif word not in data:
-        match = get_close_matches(word,data,1)
-        return data[match[0]]
+        match = get_close_matches(word,data,3)
+        print(match)
+        choice = int(input("Did you mean 1, 2 or 3 ?"))-1
+        print(choice)
+        return data[match[choice]]
 
 LookUp(word)
