a15f9096ea3f4a9015fb99101f4d876c6618b9da
Cristian <cristianejerezs.0@gmail.com>
2020-08-26 13:53:14 -0400

    Final basic program

--- DictionaryProject.py
+++ DictionaryProject.py
@@ -8,12 +8,40 @@ word = str(input("Please input a word or vague description:").casefold())
 
 def LookUp(word):
     if word in data:
-        return data[word]
-    elif word not in data:
+        return print(data[word])
+    else:
         match = get_close_matches(word,data,3)
         print(match)
-        choice = int(input("Did you mean 1, 2 or 3 ?"))-1
-        print(choice)
-        return data[match[choice]]
+        choice = input("Did you mean 1, 2 or 3 ?")
+        if int(choice)-1 <= 3:
+            return print(data[match[choice]])
+        else:
+            return "Sorry, i cant help those who cant help themselves"
 
 LookUp(word)
+
+"""
+import json
+from difflib import get_close_matches
+from difflib import SequenceMatcher
+
+data = json.load(open("DicData.json"))
+
+word = str(input("Please input a word or vague description:").casefold())
+
+def LookUp2(word):
+    if word in data:
+        return data[word]
+    elif len(get_close_matches(word, data, cutoff=0.6)) > 0:
+        choice = input("Did you mean %s instead??" % get_close_matches(word,data, cutoff=0.6)[0])
+        if choice.casefold() == "yes":
+            return str(data[get_close_matches(word,data,cutoff=0.6)[0]]).rstrip("[]')
+        else:
+            return "Sorry, please try again"
+    else:
+        return "Sorry, i can't find that word"
+
+LookUp2(word)
+
+get_close_matches()
+"""
